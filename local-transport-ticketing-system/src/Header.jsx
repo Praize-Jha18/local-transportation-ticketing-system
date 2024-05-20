c8d7562b508e62976e05be78573228003339bbff
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-05-20 22:12:29 +0100

    textfield component added to form

--- local-transport-ticketing-system/src/Header.jsx
+++ local-transport-ticketing-system/src/Header.jsx
@@ -1,27 +1,53 @@
+import { useState } from 'react';
 import './App.css'
+import { TextField } from './components/textfield';
 
 function Header() {
+
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
 
     return (
         <div className="container">
 	<div className="card w-50 mx-auto my-5">
 		<div className="card-header text-center">User Login</div>
 		<div className="card-body">
-			<form action="login" method="post">
-				<div className="form-group">
-				<label>Email</label>
-				<input className="form-control" name="email" type="email" required />
-				</div>
-				
-				<div className="form-group"> 
-				<label>Password</label>
-				<input className="form-control" name="login-password" type="password" required />
-				</div>
-				<br/>
-				<div className="text-center">
-					<button type="submit" className="btn btn-primary">Login</button>
-				</div>
-			</form>
+
+			<TextField 
+				label={"Email"} 
+				placeHolder={"Enter email address"} 
+				type={"email"}
+				defaultValue={email}
+				onChange={ 
+					(input) => { setEmail(input) }
+				}
+			/>
+
+			<TextField 
+				label={"Password"} 
+				placeHolder={"Enter Password"} 
+				type={"password"}
+				defaultValue={password}
+				onChange={ 
+					(input) => { setPassword(input) }
+				}
+			/>
+
+			{/* <TextField 
+				label={"Password"} 
+				placeHolder={"Enter Password"} 
+				type={"password"} 
+			/> */}
+
+			<br/>
+			<div className="text-center">
+				{/* <button 
+					className="btn btn-primary" 
+					onClick={
+						alert("Value is: " + email)
+					}>Login</button> */}
+			</div>
+
 		</div>
 	</div>
 </div>
