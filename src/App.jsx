5cc39b49208821dec1b3e02c65180d5c329bddee
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-05-21 11:33:21 +0100

    navigation implemented on App()

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import Header from "./Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./navbar"
+import NoPage from "./pages/no-page.page";
+// import Layout from "./pages/layout.page";
+import Home from "./pages/home.page";
+import Login from "./pages/login.page";
+
 function App() {
   return(
     <>
-      <Navbar />
-      <Header />
+      {/* <Navbar /> */}
+      {/* <Header /> */}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
