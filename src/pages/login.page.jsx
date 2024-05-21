f78f3b230e87afcf6a6e5d0fd4eb29b1b369b6b9
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-05-21 17:03:04 +0100

    refactor

--- src/pages/login.page.jsx
+++ src/pages/login.page.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-// import { TextField } from './components/textfield';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { isValidEmail } from '../utils/util';
