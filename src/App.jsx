27b5acf464c956278382937085a18f91dc9204e6
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-05-25 23:36:03 +0100

    profile-bus-booking screens added to routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import NoPage from "./pages/no-page.page";
 import Home from "./pages/home.page";
 import Login from "./pages/login.page";
 import Navbar from "./components/navbar";
+import Profile from "./pages/profile.page";
+import Bus from "./pages/bus.page";
+import Booking from "./pages/booking.page";
 
 function App() {
   return(
@@ -15,6 +18,9 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/manage-bus" element={<Bus />} />
+          <Route path="/booking" element={<Booking />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
