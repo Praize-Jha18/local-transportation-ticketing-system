b87a006892a5212692402be188c91c7dc481cf72
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-05-27 21:09:30 +0100

    state, lg added to UI

--- src/pages/sign-up.page.jsx
+++ src/pages/sign-up.page.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { isValidEmail } from '../utils/util';
 import { TextField } from '../components/textfield';
 // import axios from 'axios';
+import NaijaStates, { lgas } from 'naija-state-local-government';
+import Select from "react-select";
 import '../index.css'
 
 function SignUp() {
@@ -12,41 +14,62 @@ function SignUp() {
   const [password, setPassword] = useState("")
   const [fname, setFName] = useState("")
   const [lname, setLName] = useState("")
-  const [gender, setGender] = useState("")
+  // const [gender, setGender] = useState("")
   const [DOB, setDOB] = useState("")
   const [phoneno, setphoneno] = useState("")
   const [address, setaddress] = useState("")
   const [lga, setLGA] = useState("")
   const [state, setState] = useState("")
+  const [localGovernments, setLocalGovernments] = useState()
+  const [states, setStates] = useState()
 
+  useEffect(() => {
+    const mappedStates = []
+    NaijaStates.states().forEach(naija => mappedStates.push({ label: naija, value: naija }))
+    setStates(mappedStates)
+  }, [])
 
+  useEffect(() => {
+    if (state) {
+      setLGA("")
+      const mappedLGAs = []
+      const retrievedLgas = lgas(state.value).lgas
+      retrievedLgas.forEach(LGA => mappedLGAs.push({ label: LGA, value: LGA }))
+      setLocalGovernments(mappedLGAs)
+    }
+  }, [state])
 
-  const handleLogin = () => {
+  const validateFields = () => {
+    var isValid = false
     if (!email || !password) {
       toast.error("Field(s) can not be blank")
     } else if (!isValidEmail(email)) {
       toast.error("Invalid email address specified")
-    } else {
+    } 
+    return isValid
+  }
+
+  const handleLogin = () => {
+    if (validateFields()) {
+      // console.log(NaijaStates.all());
+      // console.log(NaijaStates.states());
+      // console.log(NaijaStates.lgas("Oyo"))
       // const build_type = process.env.BUILD_TYPE
       // console.log(build_type)
-      /*
-      axios.get(
-        `https://jsonplaceholder.typicode.com/posts`
-      )
-      .then((response) => {
-          const posts = response.data
-          console.log(posts)
-      }); 
-      */
+      // axios.get(
+      //   `https://jsonplaceholder.typicode.com/posts`
+      // )
+      // .then((response) => {
+      //     const posts = response.data
+      //     console.log(posts)
+      // });
       toast.success("On the go....")
     }
   }
 
   return (
-    // <div className='container' style={{backgroundColor: '#2c44ac', width:'100%'}}>
     <div className="container mt-5" style={{ backgroundColor: '#3036d3', padding: '30px', borderRadius: '10px', color: 'white', width:'50%', marginBottom:'20px' }}>
       <h1 className="text-center mb-4">Sign Up</h1>
-      <form>
         <div className="form-row mb-3">
           <div className="col">
             <TextField
@@ -106,12 +129,32 @@ function SignUp() {
         </div>
         <div className="form-group mb-3">
           <label htmlFor="formGender">Gender</label>
-          <select className="form-control" id="formGender">
+          <select className="form-select" id="formGender">
             <option>Gender</option>
             <option>Male</option>
             <option>Female</option>
             <option>Other</option>
           </select>
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="formGender">States</label>
+          <Select
+            className="form-control"
+            defaultValue={state}
+            onChange={setState}
+            options={states}
+            isSearchable={false}
+          />
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="formGender">Local Governments</label>
+          <Select
+            className="form-control"
+            defaultValue={lga}
+            onChange={setLGA}
+            options={localGovernments}
+            isSearchable={false}
+          />
         </div>
         <div className="form-group mb-3">
         <TextField
@@ -123,30 +166,6 @@ function SignUp() {
               (input) => { setaddress(input) }
             }
           />
-        </div>
-        <div className="form-row mb-3">
-          <div className="col">
-          <TextField
-            label={"LGA"} 
-            placeHolder={"Enter LGA"} 
-            type={"text"}
-            defaultValue={lga}
-            onChange={ 
-              (input) => { setLGA(input) }
-            }
-          />
-          </div>
-          <div className="col">
-          <TextField
-            label={"State"} 
-            placeHolder={"Enter State"} 
-            type={"text"}
-            defaultValue={state}
-            onChange={ 
-              (input) => { setState(input) }
-            }
-          />
-          </div>
         </div>
         <div className="form-group mb-3">
         <TextField
@@ -170,11 +189,10 @@ function SignUp() {
             }
           />
         </div>
-        <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#FFFFFF', color: '#4B67E3', border: 'none', marginRight:'20px', width:'150px'  }}>Sign Up</button>
-        <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#FFFFFF', color: '#4B67E3', border: 'none'  ,width:'150px' }}>Login</button>
-      </form>
+        <button className="btn btn-primary" style={{ backgroundColor: '#FFFFFF', color: '#4B67E3', border: 'none', marginRight:'20px', width:'150px'  }} onClick={ handleLogin }>Sign Up</button>
+        <button className="btn btn-primary" style={{ backgroundColor: '#FFFFFF', color: '#4B67E3', border: 'none'  ,width:'150px' }}>Login</button>
+      <ToastContainer/>
     </div>
-    // </div>
   );
 
 }
