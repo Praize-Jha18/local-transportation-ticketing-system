28a008888392d1faa3a4e8a2c3fda46fa417ef36
Praize-Jha18 <praisejahfrancis@gmail.com>
2024-05-28 14:08:12 +0100

    Edited the url for the background image on the home page

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Booking from './booking.page';
-import image from '/src/assets/Busbg.jpg'
+import image1 from '/src/assets/Busbg.jpg'
 
 const Home = () => {
   return(
-<div style={{ backgroundImage:{image}, backgroundColor:'none', backgroundSize: 'cover', height: '100vh', color: 'white' }}>
+<div style={{ backgroundImage: `url(${image1})`, backgroundColor:'none', backgroundSize: 'cover', height: '100vh', color: 'white' }}>
         <h1>WELCOME UserName</h1>
       <div className="container flex-container text-center mt-5 ">
         <div className='flex-item'>
