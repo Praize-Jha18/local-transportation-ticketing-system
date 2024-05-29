0f4db864e682159a60d1c417fcbaeac1569b5ad3
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-05-29 02:02:02 +0100

    date validator method

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,5 @@
+import { MIN_YEARS } from "../../constants";
+
 export function isValidEmail(email) {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
@@ -16,4 +18,19 @@ export function isValidAddress(address) {
 export function isValidPhone(phone) {
     const regex = /^(\+?234|0)[7-9][01]\d{8}$/;
     return regex.test(phone);
+}
+
+export function isValidAge(date) {
+    const selectedDate = new Date(date);
+    const currentDate = new Date();
+    
+    // Calculate the date MIN_YEARS=16 years ago from today
+    const minYearsAgo = new Date();
+    minYearsAgo.setFullYear(currentDate.getFullYear() - MIN_YEARS);
+    
+    // Compare the selected date with the date 16 years ago
+    if (selectedDate > minYearsAgo) {
+        return  true;
+    }
+    return false
 }
