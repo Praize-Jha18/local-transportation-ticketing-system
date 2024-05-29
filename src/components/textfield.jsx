0b2d4d365c8f1e994f1ca5e98f60f8512f25c1d4
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-05-29 01:42:26 +0100

    disabled textfield

--- src/components/textfield.jsx
+++ src/components/textfield.jsx
@@ -25,11 +25,38 @@ export const TextField = ({ label, placeHolder, type, defaultValue, onChange, di
     )
 }
 
+
+export const TextFieldDisabled = ({ label, placeHolder, type, value }) => {
+    
+    return (    
+        <div className="form-group">
+            <label>{label}</label>
+                <input 
+                    className="form-control my-2" 
+                    placeholder={placeHolder} 
+                    type={type}
+                    value={value}
+                    disabled={ true }
+                />
+        </div>
+    )
+}
+
+
 TextField.propTypes = {
     label: PropTypes.string,
+    length: PropTypes.number,
     placeHolder: PropTypes.string,
     defaultValue: PropTypes.string,
     type: PropTypes.string,
     onChange: PropTypes.func,
     disabled: PropTypes.bool
+}
+
+
+TextFieldDisabled.propTypes = {
+    label: PropTypes.string,
+    placeHolder: PropTypes.string,
+    value: PropTypes.string,
+    type: PropTypes.string
 }
