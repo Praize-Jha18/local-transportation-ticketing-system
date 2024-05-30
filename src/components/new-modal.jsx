c0013284b7ea3b6208ca58e58d7a3cf768293b63
Ibrahim Akinyele <iakinyele3@gmail.com>
2024-05-30 10:29:31 +0100

    modal refactors

--- src/components/new-modal.jsx
+++ src/components/new-modal.jsx
@@ -5,9 +5,10 @@ import { TextFieldDisabled } from './textfield';
 export const CustomModal = ({ title, message, show }) => {
   return (
     <Modal isOpen={ show } className={ "card w-50 mx-auto my-5" } style={ {backgroundColor:'#4B62E3', borderRadius:'20px'} }>
-      <div className="card-header text-center" style={{color:'white', fontSize:'25px'}}>{ title }</div>
+      <div className="card-header text-center" style={{color:'blue', fontSize:'25px'}}>{ title }</div>
       <div className="card-body">
-          <TextFieldDisabled value={ message } />
+        <center> ... { message } ... </center>
+        <TextFieldDisabled value={ message } />
       </div>
     </Modal>
   )
